8f1e481abdf88d36ae4ef246c7857edefc3e5c25
mrcdk <contact@mrcdk.com>
2021-04-13 11:23:59 +0200

    Made zlib statically linked for iOS

--- templates/ios/template/{{app.file}}/Classes/Main.mm
+++ templates/ios/template/{{app.file}}/Classes/Main.mm
@@ -3,7 +3,7 @@
 extern "C" const char *hxRunLibrary ();
 extern "C" void hxcpp_set_top_of_stack ();
 
-extern "C" int zlib_register_prims ();
+// extern "C" int zlib_register_prims ();
 extern "C" int lime_register_prims ();
 ::foreach ndlls::::if (registerStatics)::
 extern "C" int ::nameSafe::_register_prims ();::end::::end::
@@ -13,7 +13,7 @@ extern "C" int SDL_main (int argc, char *argv[]) {
 
 	hxcpp_set_top_of_stack ();
 
-	zlib_register_prims ();
+	// zlib_register_prims ();
 	lime_register_prims ();
 	::foreach ndlls::::if (registerStatics)::
 	::nameSafe::_register_prims ();::end::::end::
